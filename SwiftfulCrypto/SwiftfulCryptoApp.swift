7eaba4a3a0b2df244f5feb33e79c789f96eb8c8a
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-08-06 16:10:18 -0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Francisco Manuel Gallegos Luque on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulCryptoTests/SwiftfulCryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoTests.swift
+//  SwiftfulCryptoTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 06/08/2025.
+//
+
+import Testing
+@testable import SwiftfulCrypto
+
+struct SwiftfulCryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
